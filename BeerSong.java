5a1e5a167419c5b9989204eb26808905783a4bad
Melanie Salguero <melaniesalguero@Melanies-MacBook-Pro.local>
2019-02-07 12:46:31 -0500

    Added implementation for Ninety_Nine_Bottles_of_Beer function

--- BeerSong.java
+++ BeerSong.java
@@ -2,10 +2,14 @@
 public class BeerSong{
 
   public static void main(String args[]){
-  
+
   }
 
   public static void Ninety_Nine_Bottles_of_Beer(){
+    for(int i = 0; i < 98; i++){
+      System.out.println((99-i) + " bottles of beer on the wall,"+ (99-i) +" bottles of beer\n take one down, pass it around, " + (98-i) + " bottles of beer on the wall");
+    }
+    System.out.println((1) + " bottles of beer on the wall,"+ (1) +" bottles of beer\n take one down, pass it around, no more bottles of beer on the wall");
 
   }
 
